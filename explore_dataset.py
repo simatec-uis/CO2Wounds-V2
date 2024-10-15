c6e369aa76650f24f11358ec45b204cac48810c0
Karen Sanchez <65079093+ksanchez84@users.noreply.github.com>
2024-10-15 12:35:47 +0300

    Three files added
    Main, get_results, and explore_dataset files

---
+++ explore_dataset.py
@@ -0,0 +1,346 @@
+import os
+import json
+import random
+import numpy as np
+import requests
+from io import BytesIO
+import base64
+from math import trunc
+from PIL import Image as PILImage
+from PIL import ImageDraw as PILImageDraw
+from PIL import ImageDraw
+from PIL import ExifTags
+from PIL import ImageColor
+
+
+# Load the dataset json
+class CocoDataset():
+    def __init__(self, annotation_path, image_dir):
+        self.annotation_path = annotation_path
+        self.image_dir = image_dir
+        # self.colors = colors = ['blue', 'purple', 'red', 'green', 'orange', 'salmon', 'pink', 'gold',
+        #                         'orchid', 'slateblue', 'limegreen', 'seagreen', 'darkgreen', 'olive',
+        #                        'teal', 'aquamarine', 'steelblue', 'powderblue', 'dodgerblue', 'navy',
+        #                        'magenta', 'sienna', 'maroon']
+        self.colors = colors = ['blue']
+        
+        json_file = open(self.annotation_path)
+        self.coco = json.load(json_file)
+        json_file.close()
+        
+        self.process_info()
+        self.process_licenses()
+        self.process_categories()
+        self.process_images()
+        self.process_segmentations()
+            
+        
+    def display_info(self):
+        print('Dataset Info:')
+        print('=============')
+        for key, item in self.info.items():
+            print('  {}: {}'.format(key, item))
+        
+        requirements = [['description', str],
+                        ['url', str],
+                        ['version', str],
+                        ['year', int],
+                        ['contributor', str],
+                        ['date_created', str]]
+        for req, req_type in requirements:
+            if req not in self.info:
+                print('ERROR: {} is missing'.format(req))
+            elif type(self.info[req]) != req_type:
+                print('ERROR: {} should be type {}'.format(req, str(req_type)))
+        print('')
+
+        
+    def display_licenses(self):
+        print('Licenses:')
+        print('=========')
+        
+        requirements = [['id', int],
+                        ['url', str],
+                        ['name', str]]
+        for license in self.licenses:
+            for key, item in license.items():
+                print('  {}: {}'.format(key, item))
+            for req, req_type in requirements:
+                if req not in license:
+                    print('ERROR: {} is missing'.format(req))
+                elif type(license[req]) != req_type:
+                    print('ERROR: {} should be type {}'.format(req, str(req_type)))
+            print('')
+        print('')
+        
+    def display_categories(self):
+        print('Categories:')
+        print('=========')
+        for sc_key, sc_val in self.super_categories.items():
+            print('  super_category: {}'.format(sc_key))
+            for cat_id in sc_val:
+                print('    id {}: {}'.format(cat_id, self.categories[cat_id]['name']))
+            print('')
+    
+    def display_image(self, image_id, show_polys=True, show_bbox=True, show_crowds=True, use_url=False):
+        print('Image:')
+        print('======')
+        if image_id == 'random':
+            image_id = random.choice(list(self.images.keys()))
+        
+        # Print the image info
+        image = self.images[image_id]
+        for key, val in image.items():
+            print('  {}: {}'.format(key, val))
+            
+        # Open the image
+        if use_url:
+            image_path = image['coco_url']
+            response = requests.get(image_path)
+            image = PILImage.open(BytesIO(response.content))
+            
+        else:
+            image_path = os.path.join(self.image_dir, image['file_name'])
+            image = PILImage.open(image_path)
+            
+            buffer = BytesIO()
+            image.save(buffer, format='PNG')
+            buffer.seek(0)
+            
+            data_uri = base64.b64encode(buffer.read()).decode('ascii')
+            image_path = "data:image/png;base64,{0}".format(data_uri)
+            
+        # Calculate the size and adjusted display size
+        max_width = 600
+        image_width, image_height = image.size
+        adjusted_width = min(image_width, max_width)
+        adjusted_ratio = adjusted_width / image_width
+        adjusted_height = adjusted_ratio * image_height
+        
+        # Create list of polygons to be drawn
+        polygons = {}
+        bbox_polygons = {}
+        rle_regions = {}
+        poly_colors = {}
+        print('  segmentations ({}):'.format(len(self.segmentations[image_id])))
+        for i, segm in enumerate(self.segmentations[image_id]):
+            polygons_list = []
+            if segm['iscrowd'] != 0:
+                # Gotta decode the RLE
+                px = 0
+                x, y = 0, 0
+                rle_list = []
+                for j, counts in enumerate(segm['segmentation']['counts']):
+                    if j % 2 == 0:
+                        # Empty pixels
+                        px += counts
+                    else:
+                        # Need to draw on these pixels, since we are drawing in vector form,
+                        # we need to draw horizontal lines on the image
+                        x_start = trunc(trunc(px / image_height) * adjusted_ratio)
+                        y_start = trunc(px % image_height * adjusted_ratio)
+                        px += counts
+                        x_end = trunc(trunc(px / image_height) * adjusted_ratio)
+                        y_end = trunc(px % image_height * adjusted_ratio)
+                        if x_end == x_start:
+                            # This is only on one line
+                            rle_list.append({'x': x_start, 'y': y_start, 'width': 1 , 'height': (y_end - y_start)})
+                        if x_end > x_start:
+                            # This spans more than one line
+                            # Insert top line first
+                            rle_list.append({'x': x_start, 'y': y_start, 'width': 1, 'height': (image_height - y_start)})
+                            
+                            # Insert middle lines if needed
+                            lines_spanned = x_end - x_start + 1 # total number of lines spanned
+                            full_lines_to_insert = lines_spanned - 2
+                            if full_lines_to_insert > 0:
+                                full_lines_to_insert = trunc(full_lines_to_insert * adjusted_ratio)
+                                rle_list.append({'x': (x_start + 1), 'y': 0, 'width': full_lines_to_insert, 'height': image_height})
+                                
+                            # Insert bottom line
+                            rle_list.append({'x': x_end, 'y': 0, 'width': 1, 'height': y_end})
+                if len(rle_list) > 0:
+                    rle_regions[segm['id']] = rle_list  
+            else:
+                # Add the polygon segmentation
+                for segmentation_points in segm['segmentation']:
+                    segmentation_points = np.multiply(segmentation_points, adjusted_ratio).astype(int)
+                    polygons_list.append(str(segmentation_points).lstrip('[').rstrip(']'))
+            polygons[segm['id']] = polygons_list
+            if i < len(self.colors):
+                poly_colors[segm['id']] = self.colors[i]
+            else:
+                poly_colors[segm['id']] = 'white'
+            
+            bbox = segm['bbox']
+            bbox_points = [bbox[0], bbox[1], bbox[0] + bbox[2], bbox[1],
+                           bbox[0] + bbox[2], bbox[1] + bbox[3], bbox[0], bbox[1] + bbox[3],
+                           bbox[0], bbox[1]]
+            bbox_points = np.multiply(bbox_points, adjusted_ratio).astype(int)
+            bbox_polygons[segm['id']] = str(bbox_points).lstrip('[').rstrip(']')
+            
+            # Print details
+            print('    {}:{}:{}'.format(segm['id'], poly_colors[segm['id']], self.categories[segm['category_id']]))
+        
+        
+        
+        # Draw segmentation polygons on image
+        html  = '<div class="container" style="position:relative;">'
+        html += '<img src="{}" style="position:relative;top:0px;left:0px;width:{}px;">'.format(image_path, adjusted_width)
+        html += '<div class="svgclass"><svg width="{}" height="{}">'.format(adjusted_width, adjusted_height)
+        
+        if show_polys:
+            for seg_id, points_list in polygons.items():
+                fill_color = poly_colors[seg_id]
+                stroke_color = poly_colors[seg_id]
+                for points in points_list:
+                    html += '<polygon points="{}" style="fill:{}; stroke:{}; stroke-width:1; fill-opacity:0.5" />'.format(points, fill_color, stroke_color)
+        
+        if show_crowds:
+            for seg_id, rect_list in rle_regions.items():
+                fill_color = poly_colors[seg_id]
+                stroke_color = poly_colors[seg_id]
+                for rect_def in rect_list:
+                    x, y = rect_def['x'], rect_def['y']
+                    w, h = rect_def['width'], rect_def['height']
+                    html += '<rect x="{}" y="{}" width="{}" height="{}" style="fill:{}; stroke:{}; stroke-width:1; fill-opacity:0.5; stroke-opacity:0.5" />'.format(x, y, w, h, fill_color, stroke_color)
+            
+        if show_bbox:
+            for seg_id, points in bbox_polygons.items():
+                fill_color = poly_colors[seg_id]
+                stroke_color = poly_colors[seg_id]
+                html += '<polygon points="{}" style="fill:{}; stroke:{}; stroke-width:1; fill-opacity:0" />'.format(points, fill_color, stroke_color)
+                
+        html += '</svg></div>'
+        html += '</div>'
+        html += '<style>'
+        html += '.svgclass { position:absolute; top:0px; left:0px;}'
+        html += '</style>'
+        return html
+       
+    def process_info(self):
+        self.info = self.coco['info']
+    
+    def process_licenses(self):
+        self.licenses = self.coco['licenses']
+    
+    def process_categories(self):
+        self.categories = {}
+        self.super_categories = {}
+        for category in self.coco['categories']:
+            cat_id = category['id']
+            super_category = category['supercategory']
+            
+            # Add category to the categories dict
+            if cat_id not in self.categories:
+                self.categories[cat_id] = category
+            else:
+                print("ERROR: Skipping duplicate category id: {}".format(category))
+
+            # Add category to super_categories dict
+            if super_category not in self.super_categories:
+                self.super_categories[super_category] = {cat_id} # Create a new set with the category id
+            else:
+                self.super_categories[super_category] |= {cat_id} # Add category id to the set
+                
+    def process_images(self):
+        self.images = {}
+        for image in self.coco['images']:
+            image_id = image['id']
+            if image_id in self.images:
+                print("ERROR: Skipping duplicate image id: {}".format(image))
+            else:
+                self.images[image_id] = image
+                
+    def process_segmentations(self):
+        self.segmentations = {}
+        for segmentation in self.coco['annotations']:
+            image_id = segmentation['image_id']
+            if image_id not in self.segmentations:
+                self.segmentations[image_id] = []
+            self.segmentations[image_id].append(segmentation)
+
+        
+
+    def save_image_with_annotations(self, image_id, save_path, use_url=False, show_polys=True, show_bbox=True, show_crowds=True):
+        if image_id == 'random':
+            image_id = random.choice(list(self.images.keys()))
+
+        image_info = self.images[image_id]
+        if use_url:
+            response = requests.get(image_info['coco_url'])
+            image = PILImage.open(BytesIO(response.content))
+        else:
+            image_path = os.path.join(self.image_dir, image_info['file_name'])
+            image = PILImage.open(image_path)
+            # Use this function after opening the image but before drawing on it
+            # image = self.correct_image_orientation(image)
+
+        # Create a transparent overlay
+        overlay = PILImage.new('RGBA', image.size, (255, 255, 255, 0))
+        draw = ImageDraw.Draw(overlay)
+
+        for segm in self.segmentations[image_id]:
+            color = random.choice(self.colors)  # Choose a random color for each segmentation
+            base_color = ImageColor.getrgb(color)
+            # Adjust the 0.5 to control lightness, and add an alpha value for opacity
+            fill_color = tuple(min(255, int(c + (255 - c) * 0.5)) for c in base_color) + (128,)  # 128 is the opacity
+
+            if segm['iscrowd'] == 0 and show_polys:  # If not crowd and showing polygons
+                for segmentation_points in segm['segmentation']:
+                    # Draw each polygon with the transparent fill color
+                    polygon = [tuple(segmentation_points[i:i+2]) for i in range(0, len(segmentation_points), 2)]
+                    draw.polygon(polygon, outline=color, fill=fill_color, width=2)
+
+            if show_bbox:
+                # Draw bounding box with opacity
+                bbox = segm['bbox']
+                bbox_outline = [bbox[0], bbox[1], bbox[0] + bbox[2], bbox[1] + bbox[3]]
+                # Use the RGBA color for outline to control its opacity, if needed
+                outline_color = color #fill_color  # or define another color with opacity
+                draw.rectangle(bbox_outline, outline=outline_color, width=3)
+
+        # Composite the overlay with the original image
+        image_with_overlay = PILImage.alpha_composite(image.convert('RGBA'), overlay)
+
+        # Convert back to RGB to save as PNG if the original image is not RGBA
+        if image.mode != 'RGBA':
+            image_with_overlay = image_with_overlay.convert('RGB')
+
+        image_with_overlay.save(save_path, "PNG")
+        print(f"Image with annotations saved to {save_path}")
+
+
+if __name__ == "__main__":
+    annotation_path = './data/CO2wounds/annotations/merged_annotations.json' #'./sample_annotations.json'
+    image_dir = './data/CO2wounds/imgs' #'./images'
+
+    save_path = "./data/CO2wounds/vis/"
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    coco_dataset = CocoDataset(annotation_path, image_dir)
+    coco_dataset.display_info()
+    coco_dataset.display_licenses()
+    coco_dataset.display_categories()
+
+    # html = coco_dataset.display_image(10, use_url=False)
+    # html_file_path = 'image_display.html'
+    # with open(html_file_path, 'w') as file:
+    #     file.write(html)
+
+    # # Open the HTML file in the default web browser
+    # import webbrowser
+    # webbrowser.open_new_tab(html_file_path)
+
+    for i in range(1, 608):
+
+        image_id_to_save = i # 50  # or 'random' for a random image
+        image_info = coco_dataset.images[i]
+
+        save_file_path = os.path.join(save_path,f'imgID_{image_id_to_save}_imgFile_{image_info["file_name"].split(".")[0]}.png')
+        coco_dataset.save_image_with_annotations(image_id_to_save, save_file_path, use_url=False)
+
+    # image_id_to_save = 57 # 50  # or 'random' for a random image
+    # save_path = f'results/annotated_image_{image_id_to_save}.png'
+    # coco_dataset.save_image_with_annotations(image_id_to_save, save_path, use_url=False)
---
+++ get_results.py
@@ -0,0 +1,329 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset as BaseDataset
+import albumentations as albu
+import torch
+import numpy as np
+import segmentation_models_pytorch as smp
+from segmentation_models_pytorch.utils.train import ValidEpoch
+import segmentation_models_pytorch.utils
+from torch.optim.lr_scheduler import StepLR
+
+
+
+# helper function for data visualization
+def visualize(fig_name=None, result=False, **images):
+    """PLot images in one row."""
+    n = len(images)
+    plt.figure(figsize=(16, 5))
+    for i, (name, image) in enumerate(images.items()):
+        plt.subplot(1, n, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(' '.join(name.split('_')).title())
+        if result and i>0:
+            plt.imshow(image, cmap='gray')
+        else:
+            plt.imshow(image)
+    # plt.show()
+    if fig_name is None:
+        fig_name = "tmp.png"
+    plt.savefig(fig_name)
+
+class Dataset(BaseDataset):
+    """CO2wounds Dataset. Read images, apply augmentation and preprocessing transformations.
+    
+    Args:
+        images_dir (str): path to images folder
+        masks_dir (str): path to segmentation masks folder
+        class_values (list): values of classes to extract from segmentation mask
+        augmentation (albumentations.Compose): data transfromation pipeline 
+            (e.g. flip, scale, etc.)
+        preprocessing (albumentations.Compose): data preprocessing 
+            (e.g. normalization, shape manipulation, etc.)
+    
+    """
+    
+    CLASSES = ['background', 'wound']
+    
+    def __init__(
+            self, 
+            images_dir, 
+            masks_dir, 
+            classes=None, 
+            augmentation=None, 
+            preprocessing=None,
+    ):
+        self.ids = os.listdir(images_dir)
+        self.images = [os.path.join(images_dir, image_id) for image_id in self.ids]
+        self.masks = [os.path.join(masks_dir, image_id.split(".")[0]+".png") for image_id in self.ids]
+        
+        # convert str names to class values on masks 
+        self.class_values = [self.CLASSES.index(cls.lower()) for cls in classes]
+        
+        self.augmentation = augmentation
+        self.preprocessing = preprocessing
+    
+    def __getitem__(self, i):
+        
+        # read data
+        image = cv2.imread(self.images[i])
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        mask = cv2.imread(self.masks[i], 0)
+        
+        masks = [(mask == v) for v in self.class_values]
+        mask = np.stack(masks, axis=-1).astype('float')
+        
+        # apply augmentations
+        if self.augmentation:
+            sample = self.augmentation(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+        
+        # apply preprocessing
+        if self.preprocessing:
+            sample = self.preprocessing(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+            
+        return image, mask
+        
+    def __len__(self):
+        return len(self.ids)
+    
+
+
+def get_training_augmentation():
+    train_transform = [
+
+        albu.HorizontalFlip(p=0.5),
+
+        albu.ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0.1, p=1, border_mode=0),
+
+        albu.PadIfNeeded(min_height=320, min_width=320, always_apply=True, border_mode=0),
+        albu.RandomCrop(height=320, width=320, always_apply=True),
+
+        albu.IAAAdditiveGaussianNoise(p=0.2),
+        albu.IAAPerspective(p=0.5),
+
+        albu.OneOf(
+            [
+                albu.CLAHE(p=1),
+                albu.RandomBrightness(p=1),
+                albu.RandomGamma(p=1),
+            ],
+            p=0.9,
+        ),
+
+        albu.OneOf(
+            [
+                albu.IAASharpen(p=1),
+                albu.Blur(blur_limit=3, p=1),
+                albu.MotionBlur(blur_limit=3, p=1),
+            ],
+            p=0.9,
+        ),
+
+        albu.OneOf(
+            [
+                albu.RandomContrast(p=1),
+                albu.HueSaturationValue(p=1),
+            ],
+            p=0.9,
+        ),
+    ]
+    return albu.Compose(train_transform)
+
+
+# def get_validation_augmentation():
+#     """Add paddings to make image shape divisible by 32"""
+#     test_transform = [
+#         albu.PadIfNeeded(384, 480, )
+#     ]
+#     return albu.Compose(test_transform)
+def get_validation_augmentation():
+    """Adaptively add paddings to make image shape divisible by 32"""
+    test_transform = [
+        albu.LongestMaxSize(max_size=384, always_apply=True),
+        albu.PadIfNeeded(min_height=384, min_width=480, always_apply=True, border_mode=0, value=0),
+    ]
+    return albu.Compose(test_transform)
+
+
+def to_tensor(x, **kwargs):
+    return x.transpose(2, 0, 1).astype('float32')
+
+
+def get_preprocessing(preprocessing_fn):
+    """Construct preprocessing transform
+    
+    Args:
+        preprocessing_fn (callbale): data normalization function 
+            (can be specific for each pretrained neural network)
+    Return:
+        transform: albumentations.Compose
+    
+    """
+    
+    _transform = [
+        albu.Lambda(image=preprocessing_fn),
+        albu.Lambda(image=to_tensor, mask=to_tensor),
+    ]
+    return albu.Compose(_transform)
+
+
+
+def main():
+    ########### Load data and visualize ##################
+    DATA_DIR = './data/CO2wounds/'
+
+    # load repo with data if it is not exists
+    if not os.path.exists(DATA_DIR):
+        print('Please download our dataset')
+
+
+    x_train_dir = os.path.join(DATA_DIR, 'train')
+    y_train_dir = os.path.join(DATA_DIR, 'train_anns')
+
+    x_valid_dir = os.path.join(DATA_DIR, 'val')
+    y_valid_dir = os.path.join(DATA_DIR, 'val_anns')
+
+    ########### Create model and train ###########
+    ENCODER = 'mit_b5' #'se_resnext50_32x4d'
+    ENCODER_WEIGHTS = 'imagenet'
+    CLASSES = ['wound']
+    ACTIVATION = 'sigmoid' # could be None for logits or 'softmax2d' for multiclass segmentation
+    DEVICE = 'cuda'
+
+    # create segmentation model with pretrained encoder
+    model = smp.FPN(
+        encoder_name=ENCODER, 
+        encoder_weights=ENCODER_WEIGHTS, 
+        classes=len(CLASSES), 
+        activation=ACTIVATION,
+    )
+
+    preprocessing_fn = smp.encoders.get_preprocessing_fn(ENCODER, ENCODER_WEIGHTS)
+
+    train_dataset = Dataset(
+    x_train_dir, 
+    y_train_dir, 
+    augmentation=get_training_augmentation(), 
+    preprocessing=get_preprocessing(preprocessing_fn),
+    classes=CLASSES,
+    )
+
+    valid_dataset = Dataset(
+        x_valid_dir, 
+        y_valid_dir, 
+        augmentation=get_validation_augmentation(), 
+        preprocessing=get_preprocessing(preprocessing_fn),
+        classes=CLASSES,
+    )
+
+    train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, num_workers=12)
+    valid_loader = DataLoader(valid_dataset, batch_size=1, shuffle=False, num_workers=4)
+
+    # Dice/F1 score - https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient
+    # IoU/Jaccard score - https://en.wikipedia.org/wiki/Jaccard_index
+
+    loss = smp.utils.losses.DiceLoss()
+    metrics = [
+        smp.utils.metrics.IoU(threshold=0.5),
+        smp.utils.metrics.Fscore()
+    ]
+
+    optimizer = torch.optim.Adam([ 
+        dict(params=model.parameters(), lr=0.0001),
+    ])
+
+    scheduler = StepLR(optimizer, step_size=10, gamma=0.1)
+    starting_epoch_scheduler=40 # TODO: tune this hyperparameter
+
+    # define number of epochs
+    max_score = 0
+    num_epochs = 0
+
+    # create epoch runners 
+    # it is a simple loop of iterating over dataloader`s samples
+    train_epoch = smp.utils.train.TrainEpoch(
+        model, 
+        loss=loss, 
+        metrics=metrics, 
+        optimizer=optimizer,
+        device=DEVICE,
+        verbose=True,
+    )
+
+    valid_epoch = smp.utils.train.ValidEpoch(
+        model, 
+        loss=loss, 
+        metrics=metrics, 
+        device=DEVICE,
+        verbose=True,
+    )
+
+
+    ########### Test best saved model ###########
+    # load best saved checkpoint
+    best_model = torch.load('./best_checkpoint.pth')
+
+
+    test_dataloader = DataLoader(valid_dataset)
+    # evaluate model on test set
+    metrics_test= metrics = [
+        smp.utils.metrics.IoU(threshold=0.5),
+        smp.utils.metrics.Fscore(),
+        smp.utils.metrics.Accuracy(),
+        smp.utils.metrics.Precision(),
+        smp.utils.metrics.Recall()
+    ]
+    test_epoch = ValidEpoch(
+        model=best_model,
+        loss=loss,
+        metrics=metrics_test,
+        device=DEVICE,
+        verbose=True,
+    )
+
+    logs = test_epoch.run(test_dataloader)
+
+    print(f">>Logs: {logs}")
+
+    ########### Visualize predictions ###########
+    # test dataset without transformations for image visualization
+    test_dataset_vis = Dataset(
+        x_valid_dir, y_valid_dir, 
+        classes=CLASSES,
+    )
+
+    ids_show = [0, 7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98, 105, 112, 119, 5, 10]
+    for i, n in enumerate(ids_show):
+        
+        image_vis = test_dataset_vis[n][0].astype('uint8')
+        image, gt_mask = valid_dataset[n]
+        
+        gt_mask = gt_mask.squeeze()
+        
+        x_tensor = torch.from_numpy(image).to(DEVICE).unsqueeze(0)
+        pr_mask = best_model.predict(x_tensor)
+        pr_mask = (pr_mask.squeeze().cpu().numpy().round())
+            
+        visualize(
+            "result_"+str(i)+".png",
+            result=True,
+            image=image_vis, 
+            ground_truth_mask=gt_mask, 
+            predicted_mask=pr_mask
+        )
+    
+
+
+if __name__ == "__main__":
+    main()
+
+
